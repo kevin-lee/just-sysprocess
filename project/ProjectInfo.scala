9091dbe6d68602d8cf6b2e16d396fcbb30138dfc
Kevin Lee <kevin.code@kevinlee.io>
2020-08-15 14:53:36 +1000

    Prepare to release v0.2.0

--- project/ProjectInfo.scala
+++ project/ProjectInfo.scala
@@ -3,13 +3,13 @@ import wartremover.WartRemover.autoImport.{Wart, Warts}
 object ProjectInfo {
   final case class ProjectName(projectName: String) extends AnyVal
 
-  val ProjectVersion: String = "0.1.0"
+  val ProjectVersion: String = "0.2.0"
 
   def commonWarts(scalaBinaryVersion: String): Seq[wartremover.Wart] = scalaBinaryVersion match {
-    case "2.10" =>
-      Seq.empty
-    case _ =>
+    case "2.13" | "2.12" | "2.11" =>
       Warts.all
+    case _ =>
+      Seq.empty
   }
 
 }
